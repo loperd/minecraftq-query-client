2578ee4f797374b2329afb6d6d6340137f73d182
loper <bizrenay@gmail.com>
2023-05-14 21:55:11 +0300

    upgrade domain regex

--- src/Loper/MinecraftQueryClient/Address/ServerAddress.php
+++ src/Loper/MinecraftQueryClient/Address/ServerAddress.php
@@ -8,7 +8,7 @@ use Loper\MinecraftQueryClient\Exception\ServerAddressResolveException;
 
 final class ServerAddress
 {
-    public const DOMAIN_REGEX = '/^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9])|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9]{2,30}\.[a-zA-Z]{2,3})$/';
+    public const DOMAIN_REGEX = '/^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/';
     public const IP_REGEX = '/^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/';
     public const DEFAULT_SERVER_PORT = 25565;
 
